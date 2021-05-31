ed891022de828c6e95bdd1e23aaf2bf8b5a841b6
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-05-31 16:45:55 -0400

    Update seed.py

--- seed.py
+++ seed.py
@@ -13,7 +13,7 @@ Post.query.delete()
 first_user = User(first_name='Tom', last_name='Smith', image_url='https://i.pinimg.com/originals/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107.png')
 second_user = User(first_name='Jack', last_name='Sparrow', image_url='https://iadsb.tmgrup.com.tr/0999d9/0/0/0/0/2048/1199?u=https://idsb.tmgrup.com.tr/2017/03/19/jack-sparrow-might-be-inspired-by-a-muslim-captain-1489951367309.jpg')
 
-# Add postes
+# Add posts
 post_1 = Post(title='First post title', content='This text of the fisrt post', user_id=1)
 post_2 = Post(title='Second post title', content='This text of the second post', user_id=2)
 post_3 = Post(title='Third post title', content='This text of the third post', user_id=1)
